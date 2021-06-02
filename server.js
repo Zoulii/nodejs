c820c4ce5c01d13cb269cffe563e6c06d6f65947
Zulikhan USUMOVA <Zulikhan.USUMOVA@brussels.msf.org>
2021-06-02 11:36:52 +0200

    importing body parser adding .envconfig

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+//Importing packages into our file
+//Import name from package name, is the same like const name = require('package name')
 import express from "express";
+import bodyParser from "body-parser";
 
-const app = expres();
+
+//To be able to use variables in .env file - without it it will be undefined 
+require(".env").config();
+
+//Initializing the server
+const app = express();
