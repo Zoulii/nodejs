8115d89876d6bc46710640913d0e9ceeec2a60c1
Zulikhan USUMOVA <Zulikhan.USUMOVA@brussels.msf.org>
2021-06-02 14:15:07 +0200

    connecting db

---
+++ src/config/database.js
@@ -0,0 +1,19 @@
+import { Pool } from "pg";
+
+// Function from postgre , finding new pool of DB, connexion between our application and this DB
+// 
+require("dotenv").config();
+const pool = new Pool({
+    connectionString: process.send.DATABASE_URL,
+});
+
+//Console.log a successful DB connection msg
+pool.on("connect", () => {
+    console.log("Connection with the DB has been etablished");
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+};
+
+//Query is an object
