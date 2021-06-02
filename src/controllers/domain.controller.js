024c1b5d6a830c69d4232c32cd0ef146d90ee44c
Zulikhan USUMOVA <Zulikhan.USUMOVA@brussels.msf.org>
2021-06-02 14:44:45 +0200

    adding domain controller

--- src/controllers/domain.controller.js
+++ src/controllers/domain.controller.js
@@ -1,1 +1,9 @@
 // Will have all the controllers of our domain.
+import db from "../config/database";
+
+exports.allDomains = async (req, res) => {
+    const { rows } = await db.query("SELECT * FROM t_domain");
+    res.status(200).send({
+        body: rows,
+    });
+};
